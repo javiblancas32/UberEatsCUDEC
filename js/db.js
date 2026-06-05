61dc2c1108fd229e4383dd70e9b5f1b8b5d31f53
Gael729 <lopezfloresc422@gmail.com>
2026-06-04 19:51:48 -0600

    Se hizo la conexion a la base de datos

--- js/db.js
+++ js/db.js
@@ -1,13 +1,5 @@
 db.collection("platillos").onSnapshot((datos) => {
     datos.forEach((registro) => {
-<<<<<<< HEAD
-        console.log(registro);
+        
     });
-
-
-=======
-        console.log(registro);                    
-    });
-
->>>>>>> 5f807f2d4ab1f26eedf68f9e1bc53ce6ea93047c
 });
